b8a6a6fb0b1473fe16745ea06a25c4c01da673e2
Masom H <94949987+masomh-personal@users.noreply.github.com>
2024-09-05 18:30:06 -0400

    StringBuilder Class: renamed a few variable names

--- OODesign/StringBuilder.js
+++ OODesign/StringBuilder.js
@@ -1,33 +1,34 @@
 export default class StringBuilder {
   // Private array to store string parts
-  #strings;
+  #stringArr;
 
   // Constructor to initialize the StringBuilder with an optional initial value
   constructor(initialValue = '') {
     // If initialValue is provided, initialize with it; otherwise, start with an empty array
-    this.#strings = initialValue ? [initialValue] : [];
+    this.#stringArr = initialValue ? [initialValue] : [];
   }
 
   // Append a new value to the end of the string
   append(value) {
-    this.#strings.push(value); // Add the value to the array
+    this.#stringArr.push(value); // Add the value to the array
     return this; // Return 'this' to enable method chaining
   }
 
   // Prepend a new value to the beginning of the string
   prepend(value) {
-    this.#strings.unshift(value); // Insert the value at the start of the array
+    // NOTE: if we have a scenario where we have LOTS of calls to the prepend method, we'd use a queue (SLL)
+    this.#stringArr.unshift(value); // Insert the value at the start of the array
     return this; // Return 'this' to enable method chaining
   }
 
   // Convert the accumulated string parts to a single string
-  printString(delimeter = '') {
-    return this.#strings.join(delimeter); // Join the array elements into a single string
+  printString(separator = '') {
+    return this.#stringArr.join(separator); // Join the array elements into a single string
   }
 
   // Clear the string builder by resetting the internal array
   clear() {
-    this.#strings = []; // Empty the array
+    this.#stringArr = []; // Empty the array
     return this; // Return 'this' for chaining
   }
 
