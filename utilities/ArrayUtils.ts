f61929fc8a6a98a7da65e0ad1976c585b8da0c4b
Masom H <94949987+masomh-personal@users.noreply.github.com>
2024-10-22 12:48:27 -0400

    ADD: ArrayUtils class that will have array specific static utility functions to be used across my project

---
+++ utilities/ArrayUtils.ts
@@ -0,0 +1,26 @@
+export class ArrayUtils {
+  /**
+   * Creates a random INTEGER array of size 'size' in no particular order.
+   * Defaults:
+   * - size: 10
+   * - intCeiling: size * 10
+   *
+   * @param size - The size of the array (default: 10).
+   * @param intCeiling - The upper bound for the random integers (default: size * 10).
+   * @returns An array of random integers.
+   */
+  static createRandomIntegerArr = (size: number = 10, intCeiling: number = size * 10): number[] => {
+    const MAX_ARR_SIZE: number = 1e6;
+    if (size > MAX_ARR_SIZE) {
+      throw new RangeError(`Provided size: ${size} is too large and may cause stack overflow`);
+    }
+
+    const arr: number[] = new Array(size);
+
+    for (let i = 0; i < size; i++) {
+      arr[i] = Math.floor(Math.random() * intCeiling);
+    }
+
+    return arr;
+  };
+}
